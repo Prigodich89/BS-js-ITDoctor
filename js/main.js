e14a35e61115099cf81bf278737c6c5f612a7616
Prigodich89 <Prigodich89@mail.ru>
2023-08-24 21:32:34 +0500

    Изменения от 24.08.23

---
+++ js/main.js
@@ -0,0 +1,29 @@
+if(!localStorage.getItem('goods')) {
+    localStorage.setItem('goods', JSON.stringify([]))
+}
+
+let myModal = new bootstrap.Modal(document.getElementById('exampleModal'), {
+    keyboard: false
+})
+
+document.querySelector('button.add-new').addEventListener('click', function(e) {
+    let name = document.getElementById('good-name').value 
+    let price = document.getElementById('good-price').value 
+    let count = document.getElementById('good-count').value 
+    if(name && price && count) {
+         document.getElementById('good-name').value = ''
+         document.getElementById('good-price').value = ''
+         document.getElementById('good-count').value = '1'
+         let goods = JSON.parse(localStorage.getItem('goods'))
+         goods.push(['good-'+goods.length, name, price,count, 0, 0, 0])
+         localStorage.setItem('goods', JSON.stringify(goods))
+         //update-goods()
+         myModal.hide()
+    } else {
+        Swal.fire({
+            icon: 'error',
+            title: 'Ошибка...',
+            text: 'Пожалуйстаб заполните все поля!',
+        })
+    }
+})
